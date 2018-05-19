f366dda8d73fb17357b67765bcb04e10aa382905
Dario Vladovic <d.vladimyr@gmail.com>
2018-05-19 02:49:25 +0200

    Implement -w, --web flag

--- cli.js
+++ cli.js
@@ -4,11 +4,26 @@
 const { EOL } = require('os');
 const { Machine } = require('braincrunch');
 const { readFileSync } = require('fs');
+const argv = require('minimist')(process.argv.slice(2));
 const path = require('path');
-
-const write = n => process.stdout.write(String.fromCharCode(n));
+const opn = require('opn');
 
 const code = readFileSync(path.join(__dirname, './brainfuck'), 'utf8');
-const machine = new Machine({ code, write });
-machine.run();
-process.stdout.write(EOL);
+const write = n => process.stdout.write(String.fromCharCode(n));
+
+if (argv.w || argv.web) {
+  const url = `https://copy.sh/brainfuck/?c=${encode(code)}`;
+  opn(url);
+} else {
+  const machine = new Machine({ code, write });
+  machine.run();
+  process.stdout.write(EOL);
+}
+
+function encode(code) {
+  return Buffer.from(code)
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '$');
+}
